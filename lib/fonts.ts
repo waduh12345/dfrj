e65ab201ef40b659fc0cce0345867e025dea339b
akmlrafly <akmalrafly875@gmail.com>
2025-09-08 10:24:56 +0700

    add lib fonts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,11 +3,11 @@ import { Fredoka, Sniglet } from "next/font/google";
 export const fredoka = Fredoka({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
-  display: "swap", // untuk performance yang lebih baik
+  display: "swap",
 });
 
 export const sniglet = Sniglet({
   subsets: ["latin"],
   weight: ["400"],
-  display: "swap", // untuk performance yang lebih baik
+  display: "swap",
 });
